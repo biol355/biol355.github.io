1cc6dcebf9cf7c8ea3677330d2c1cd4cbe1bad91
Jarrett Byrnes <jebyrnes@Jarretts-Air-2.cable.rcn.com>
2020-01-29 09:45:53 -0500

    Cleanup

---
+++ 0_load_packages.R
@@ -0,0 +1,8 @@
+#packages as of 20200112
+install.packages(c("tidyverse", "ggplot2", "ggridges", "sf", "raster", "rgdal",
+                   "broom", "sp", "nycflights13", "listviewer", "mapview", "leaflet",
+                   "rrricanesdata", "car", "lme4", "glmmTMB", "brms", "ggmap", "mapdata",
+                   "visreg", "MASS", "emmeans", "readxl", "readr", "gstat", "rgeos", "spdplyr",
+                   "gridExtra", "maptools", "visdat", "devtools", "cowplot", "ggthemes", "repurrrsive",
+                   "rethinking", "gganimate", "mvtnorm", "magrittr","RColorBrewer", "viridis",
+                   "patchwork"))
